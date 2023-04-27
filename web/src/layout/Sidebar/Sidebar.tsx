0fd22b1a7c289755c9b20b947f8f1a7d76b80cb8
atulmetanext <atul@getmetanext.com>
2023-04-27 16:39:45 +0530

    feat: add layout component sidebar

---
+++ web/src/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,53 @@
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import theme from "../../theme/theme";
+import { SidebarMenu } from "./menu.constants";
+import { SidebarBtn } from "./Components";
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          background: "#051D34",
+          color: theme.palette.primary.contrastText,
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar>
+        <img src="/lumian.svg" alt="Lumian" />
+        <Typography
+          variant="h4"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 1, ml: 2 }}
+        >
+          Lumian
+        </Typography>
+      </Toolbar>
+      <Divider
+        sx={{
+          backgroundColor: theme.palette.primary.light,
+          margin: "0 16px",
+        }}
+      />
+      <List>
+        {SidebarMenu.map((item) => (
+          <SidebarBtn item={item} key={item.id} />
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
+}
