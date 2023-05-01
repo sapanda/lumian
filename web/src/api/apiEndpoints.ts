7f91f0fc9a5b6501aa136bcec3fbffcbc57ab7ff
atulmetanext <atul@getmetanext.com>
2023-05-01 09:09:45 +0530

    feat: api end points setup

---
+++ web/src/api/apiEndpoints.ts
@@ -0,0 +1,11 @@
+const baseApiUrl = "http://localhost:8000/";
+
+const authEndPoints = {
+  login: "api/user/token/",
+};
+
+const userEndPoints = {
+  me: "api/user/me/",
+};
+
+export { baseApiUrl, authEndPoints, userEndPoints };
