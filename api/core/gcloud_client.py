7fb23cf4c0048a2d5979e9d8d7a4c264f51844de
Saswat Panda <sapanda88@gmail.com>
2023-05-01 13:24:29 -0700

    Fixing gcloud_client to obtain correct API service endpoint

--- api/core/gcloud_client.py
+++ api/core/gcloud_client.py
@@ -1,13 +1,11 @@
 from abc import ABC, abstractmethod
 import grpc
-from google import auth
 from google.api_core import exceptions
+from google.cloud.run_v2.services.services.client import ServicesClient
 from google.cloud.tasks_v2 import CloudTasksClient
 from google.cloud.tasks_v2.services.cloud_tasks.transports \
     import CloudTasksGrpcTransport
 from google.cloud.tasks_v2.types import HttpMethod
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 from app import settings
 
@@ -31,34 +29,22 @@ class GCloudClient(GCloudClientInterface):
         self.location = location
         self.queue_name = queue_name
         self.service_name = service_name
+        self.tasks_client = CloudTasksClient()
+        self.run_client = ServicesClient()
         self.service_url = self._get_cloud_run_service_url()
-        self.client = CloudTasksClient()
 
-    def _get_cloud_run_service_url(self):
+    def _get_cloud_run_service_url(self) -> str:
         """Generate the Cloud Run service URL."""
-        # Use Application Default Credentials
-        creds, _ = auth.default()
-
-        # Create the Cloud Run API client
-        cloud_run = build("run", "v1", credentials=creds)
-
-        # Get the service's URL
-        try:
-            service = cloud_run.namespaces().services().get(
-                name=(f"namespaces/{self.project_id}/services/"
-                      f"{self.service_name}")
-            ).execute()
-            return service["status"]["url"]
-        except HttpError as error:
-            print(f"Error getting Cloud Run service URL: {error}")
-            return None
+        name = f"projects/{self.project_id}/locations/{self.location}/services/{self.service_name}"
+        response = self.run_client.get_service(name=name)
+        return response.uri
 
     def create_task(self, path, payload):
         """Create a Google Cloud Task task for a given service."""
         # Create the task queue path
-        parent = self.client.queue_path(self.project_id,
-                                        self.location,
-                                        self.queue_name)
+        parent = self.tasks_client.queue_path(self.project_id,
+                                              self.location,
+                                              self.queue_name)
 
         # Get the Cloud Run service URL
         service_url = self._get_cloud_run_service_url()
@@ -67,7 +53,7 @@ class GCloudClient(GCloudClientInterface):
         task = {
             "http_request": {
                 "http_method": HttpMethod.POST,
-                "url": f"{service_url}/{path}",
+                "url": f"{service_url}{path}",
                 "headers": {
                     "Content-Type": "application/json",
                 },
@@ -76,7 +62,7 @@ class GCloudClient(GCloudClientInterface):
         }
 
         try:
-            response = self.client.create_task(
+            response = self.tasks_client.create_task(
                 request={"parent": parent, "task": task}
             )
             print(f"Task created: {response.name}")
@@ -106,7 +92,7 @@ class GCloudEmulatorClient(GCloudClientInterface):
     def create_task(self, path, payload):
         task = {
             'http_request': {
-                'http_method': 'POST',
+                'http_method': HttpMethod.POST,
                 'url': f"{self.service_url}/{path}",
                 "headers": {
                     "Content-Type": "application/json",
