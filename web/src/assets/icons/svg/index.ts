12e135f46d528b91424ac7b74183fe7a0ce1850a
atulmetanext <atul@getmetanext.com>
2023-05-01 10:02:47 +0530

    Add : assets

--- web/src/assets/icons/svg/index.ts
+++ web/src/assets/icons/svg/index.ts
@@ -1,3 +1,5 @@
+import email_icon from "./email_icon.svg";
+import lock_icon from "./lock_icon.svg";
 import projects_icon from "./projects_icon.svg";
 
-export { projects_icon };
+export { email_icon, lock_icon, projects_icon };
