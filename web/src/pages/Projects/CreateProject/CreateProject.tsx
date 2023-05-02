1fe8496b63d8e902bd50024457b0b3872bf4c37f
atulmetanext <atul@getmetanext.com>
2023-05-02 21:32:51 +0530

    feat: create project module

--- web/src/pages/Projects/CreateProject/CreateProject.tsx
+++ web/src/pages/Projects/CreateProject/CreateProject.tsx
@@ -1,49 +1,91 @@
 import { Button, Stack, Typography } from "@mui/material";
+import { PrivateContainer } from "../../../components/Containers";
+import { projects_icon } from "../../../assets/icons/svg";
+import { LabelInputCombo } from "../../../components/molecules";
+import useCreateProject from "./useCreateProject";
 
 export default function CreateProject() {
+  const { errors, handleChange, handleSave, state } = useCreateProject();
   return (
-    <Stack
-      sx={{
-        flex: 1,
-        height: "100%",
-        alignItems: "center",
-      }}
-    >
-      <Stack sx={{ maxWidth: "360px", marginTop: "10%" }}>
-        <Stack gap="20px">
-          <Typography variant="h4">Let's get started</Typography>
-          <Stack>
-            <Typography variant="body1" fontWeight="bold">
-              Connect your conferencing app (optional)
-            </Typography>
+    <PrivateContainer title="Projects" icon={projects_icon}>
+      <Stack
+        sx={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Stack
+          sx={{
+            minWidth: "600px",
+            maxWidth: "600px",
+            gap: "20px",
+          }}
+        >
+          <Typography variant="h1">Create a New Project</Typography>
 
-            <Typography variant="body1">
-              <i>
-                Select from Zoom, Teams, Meet, Webex and more to get automated
-                transcripts at the end of a call
-              </i>
-            </Typography>
-            <div style={{ marginTop: "8px" }}>
-              <Button variant="contained">Connect App</Button>
-            </div>
-          </Stack>
-          <Stack>
-            <Typography variant="body1" fontWeight="bold">
-              Create a new project
-            </Typography>
+          <LabelInputCombo
+            label="Name"
+            onChange={handleChange}
+            value={state.name}
+            placeholder="Name of the Project"
+            name="name"
+            error={errors.name}
+          />
 
-            <Typography variant="body1">
-              <i>
-                You’ll need to create a project to get started with processing
-                interviews
-              </i>
-            </Typography>
-            <div style={{ marginTop: "8px" }}>
-              <Button variant="contained">New Project</Button>
-            </div>
+          <LabelInputCombo
+            label="Goal"
+            inputDescription="Describe briefly what you’re looking to achieve with the project"
+            onChange={handleChange}
+            value={state.email}
+            placeholder=""
+            name="goal"
+            error={errors.email}
+          />
+
+          <LabelInputCombo
+            label="Old Password"
+            inputDescription="These are the questions you’d like us to answer for you. List each question on a new line."
+            onChange={handleChange}
+            value={state.oldPassword}
+            placeholder="Old Password"
+            name="oldPassword"
+            error={errors.oldPassword}
+            type="password"
+          />
+
+          <LabelInputCombo
+            label="New Password"
+            onChange={handleChange}
+            value={state.newPassword}
+            placeholder="New Password"
+            name="newPassword"
+            error={errors.newPassword}
+            type="password"
+          />
+
+          <Stack sx={{ flexDirection: "row", gap: "12px" }}>
+            <Button
+              variant="contained"
+              sx={{
+                width: "100px",
+              }}
+              onClick={handleSave}
+            >
+              Save
+            </Button>
+            <Button
+              variant="text"
+              sx={{
+                width: "100px",
+              }}
+            >
+              Cancel
+            </Button>
           </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </PrivateContainer>
   );
 }
