886a821fc0a114e0ca3ae3fb35688caed16f973e
atulmetanext <atul@getmetanext.com>
2023-05-09 18:08:34 +0530

    feat: private app bar segregation for improved reusability

--- web/src/layout/PrivateAppbar/PrivateAppbar.tsx
+++ web/src/layout/PrivateAppbar/PrivateAppbar.tsx
@@ -1,34 +1,62 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 
 const drawerWidth = 240;
 
 interface PrivateAppbarProps {
   title: string;
   icon: string;
+  subtitle?: string;
+  children?: React.ReactNode;
 }
 
-function appBarLabel(label: string, icon: string) {
+function appBarLabel(title: string, icon: string, subtitle?: string) {
   return (
     <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-        <img src={icon} alt="Projects" />
-      </IconButton>
-      <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-        {label}
-      </Typography>
+      <Stack
+        sx={{
+          flexDirection: "row",
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2, p: 0 }}
+        >
+          <img src={icon} alt="Projects" />
+        </IconButton>
+        <Stack>
+          <Typography variant="h2" noWrap component="div" sx={{ flexGrow: 1 }}>
+            {title}
+          </Typography>
+          {!!subtitle && (
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              {subtitle}
+            </Typography>
+          )}
+        </Stack>
+      </Stack>
     </Toolbar>
   );
 }
 
 export default function PrivateAppbar(props: PrivateAppbarProps) {
-  const { title, icon } = props;
+  const { children, title, icon, subtitle } = props;
   return (
     <AppBar
       position="fixed"
       color="inherit"
       sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
     >
-      <Toolbar>{appBarLabel(title, icon)}</Toolbar>
+      <Stack direction="row" gap="20px">
+        <Toolbar>{appBarLabel(title, icon, subtitle)}</Toolbar>
+        {!!children && children}
+      </Stack>
     </AppBar>
   );
 }
