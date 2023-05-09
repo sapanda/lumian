cf07677b5bccda6913e7afab78d0d8564e03ad9d
atulmetanext <atul@getmetanext.com>
2023-05-09 18:07:11 +0530

    build: tailwind config updated

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    textColor: {
+      primary: "#5B5B5B",
+    },
   },
   plugins: [],
 };
