f9704e360ffb7b42f427ff62fd25eeadae60f469
atulmetanext <atul@getmetanext.com>
2023-05-17 18:20:40 +0530

    feat: atoms to tailwind

--- web/src/components/atoms/DescriptionText/DescriptionText.tsx
+++ web/src/components/atoms/DescriptionText/DescriptionText.tsx
@@ -1,19 +1,10 @@
-import { Typography } from "@mui/material";
-
 interface DescriptionTextProps {
   children?: React.ReactNode;
 }
 export default function DescriptionText({ children }: DescriptionTextProps) {
   return (
-    <Typography
-      component="span"
-      sx={{
-        marginTop: "4px!important",
-        fontSize: "12px",
-        fontWeight: 400,
-      }}
-    >
+    <span className="text-12-400 text-gray-500">
       <i>{children}</i>
-    </Typography>
+    </span>
   );
 }
