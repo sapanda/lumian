715414894eba5e56989b9f5d429fff6f3133dbbd
atulmetanext <atul@getmetanext.com>
2023-06-01 10:02:39 +0530

    feat: apis realted to project added

--- web/src/api/projectApi.tsx
+++ web/src/api/projectApi.tsx
@@ -1,6 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "./api";
 import { baseApiUrl, projectEndpoints } from "./apiEndpoints";
+import { useNavigate } from "react-router-dom";
+import { PROJECTS } from "../router/routes.constant";
 
 interface ProjectType {
   id: number;
@@ -8,6 +10,13 @@ interface ProjectType {
   questions: string[];
   title: string;
 }
+
+interface ProjectPayloadType {
+  title: string;
+  goal: string;
+  questions: string[];
+}
+
 const getProjects = async () => {
   const res = await axiosInstance.get(
     `${baseApiUrl}${projectEndpoints.projectList}`
@@ -24,10 +33,98 @@ const getProjects = async () => {
   return transformedData;
 };
 
+const getProject = async (projectId: number | undefined) => {
+  const res = await axiosInstance.get(
+    `${baseApiUrl}${projectEndpoints.projectList}${projectId}/`
+  );
+
+  const transformedData = {
+    id: res.data.id,
+    name: res.data.title,
+    goal: res.data.goal,
+    questions: res.data.questions,
+  };
+  return transformedData;
+};
+
+const createProject = async (payload: ProjectPayloadType) => {
+  return await axiosInstance.post(
+    `${baseApiUrl}${projectEndpoints.projectList}`,
+    payload
+  );
+};
+
+const updateProject = async (
+  payload: ProjectPayloadType,
+  projectId: number
+) => {
+  return await axiosInstance.patch(
+    `${baseApiUrl}${projectEndpoints.projectList}${projectId}/`,
+    payload
+  );
+};
+
 const useGetProjectsQuery = () => {
   return useQuery(["projects"], getProjects, {
     staleTime: 1000 * 60 * 30, // 30 minutes
   });
 };
 
-export { useGetProjectsQuery };
+const useGetProjectMutation = () => {
+  return useMutation((projectId: number) => getProject(projectId), {
+    onSuccess: (data) => {
+      return data;
+    },
+  });
+};
+const useGetProjectQuery = (project_id: number | undefined) => {
+  return useQuery(["project"], () => getProject(project_id), {
+    enabled: !!project_id,
+    staleTime: 1000 * 60 * 30, // 30 minutes
+  });
+};
+
+const useCreateProjectMutation = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation((payload: ProjectPayloadType) => createProject(payload), {
+    onSuccess: () => {
+      alert(`Project created Successfully`);
+      queryClient.invalidateQueries(["projects"]);
+      setTimeout(() => {
+        navigate(PROJECTS.default);
+      }, 500);
+    },
+  });
+};
+
+const useUpdateProjectMutation = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation(
+    ({
+      payload,
+      projectId,
+    }: {
+      payload: ProjectPayloadType;
+      projectId: number;
+    }) => updateProject(payload, projectId),
+    {
+      onSuccess: () => {
+        alert(`Project updated Successfully`);
+        queryClient.invalidateQueries(["projects"]);
+        setTimeout(() => {
+          navigate(PROJECTS.default);
+        }, 500);
+      },
+    }
+  );
+};
+
+export {
+  useGetProjectsQuery,
+  useGetProjectMutation,
+  useGetProjectQuery,
+  useCreateProjectMutation,
+  useUpdateProjectMutation,
+};
