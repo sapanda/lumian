0497cfecedd4647c4aaea39c818195c29c49c813
Saswat Panda <sapanda88@gmail.com>
2023-06-04 19:44:47 +0000

    Minor change to FastAPI log formats

--- api-synthesis/app/server.py
+++ api-synthesis/app/server.py
@@ -50,7 +50,7 @@ if settings.deploy_mode == ModeEnum.development or \
         traces_sample_rate=1.0,
     )
 
-LOG_FORMAT = "%(asctime)s [%(levelname)-6s] %(name)s: %(message)s | %(funcName)s() L%(lineno)-4d" # noqa
+LOG_FORMAT = "[%(levelname)s] %(message)s | %(filename)s %(funcName)s() Line %(lineno)d" # noqa
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 logger = logging.getLogger()
 
