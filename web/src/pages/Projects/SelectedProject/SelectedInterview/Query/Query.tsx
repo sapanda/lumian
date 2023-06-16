878f3b1e488049a52c46a6b6709ec3f629a6f51e
atulmetanext <atul@getmetanext.com>
2023-06-16 10:56:12 +0530

    feat:Implement Interview Loading

--- web/src/pages/Projects/SelectedProject/SelectedInterview/Query/Query.tsx
+++ web/src/pages/Projects/SelectedProject/SelectedInterview/Query/Query.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from "@mui/material";
+import { CircularProgress, Paper, Stack } from "@mui/material";
 import theme from "../../../../../theme/theme";
 
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
@@ -51,54 +51,65 @@ export default function Query() {
           maxHeight: "75vh",
         }}
       >
-        <div className="flex flex-col flex-1 w-full gap-4 p-2 overflow-y-auto">
-          <AnswerBox
-            answer={[
-              {
-                text: "What would you like to ask?",
-                references: [],
-              },
-            ]}
-          />
-          {query?.map((item: queryProps, index: number) => {
-            return (
-              <Stack
-                sx={{
-                  gap: "15px",
-                }}
-              >
-                <QuestionBox question={item.query} />
-                <AnswerBox
-                  answer={item.output}
-                  handleSummaryItemClick={handleSummaryItemClick}
-                  selectedIndex={selectedIndex}
-                  queryIndex={index}
-                />
-              </Stack>
-            );
-          })}
-          {isAsking && (
-            <div className="flex w-full">
-              <ChatLoader />
+        {!query ? (
+          <div className="flex items-center gap-5">
+            <CircularProgress />
+            <span className="italic text-gray-500 text-12-400">
+              Querying will be available in a few minutes...
+            </span>
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col flex-1 w-full gap-4 p-2 overflow-y-auto">
+              <AnswerBox
+                answer={[
+                  {
+                    text: "What would you like to ask?",
+                    references: [],
+                  },
+                ]}
+              />
+              {query?.map((item: queryProps, index: number) => {
+                return (
+                  <Stack
+                    sx={{
+                      gap: "15px",
+                    }}
+                  >
+                    <QuestionBox question={item.query} />
+                    <AnswerBox
+                      answer={item.output}
+                      handleSummaryItemClick={handleSummaryItemClick}
+                      selectedIndex={selectedIndex}
+                      queryIndex={index}
+                    />
+                  </Stack>
+                );
+              })}
+              {isAsking && (
+                <div className="flex w-full">
+                  <ChatLoader />
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        <div className="flex flex-col w-full p-2">
-          <QueryInput
-            placeholder="Enter your query"
-            onChange={(e) => {
-              setUserQueryText(e.target.value);
-            }}
-            value={userQueryText}
-            onSend={askQuery}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault();
-                askQuery();
-              }
-            }}
-          />
-        </div>
+            <div className="flex flex-col w-full p-2">
+              <QueryInput
+                placeholder="Enter your query"
+                onChange={(e) => {
+                  setUserQueryText(e.target.value);
+                }}
+                value={userQueryText}
+                onSend={askQuery}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    askQuery();
+                  }
+                }}
+              />
+            </div>
+          </>
+        )}
       </Paper>
 
       <Paper
