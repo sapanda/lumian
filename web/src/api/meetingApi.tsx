47160712cfe5645fde020841e6e2658d2966ae0e
atulmetanext <atul@getmetanext.com>
2023-06-21 23:05:49 +0530

    feat: transcribe on focus api call fix

--- web/src/api/meetingApi.tsx
+++ web/src/api/meetingApi.tsx
@@ -316,6 +316,7 @@ const useCalendarStatusQuery = () => {
   return useQuery(queryKey, () => getCalendarStatus(), {
     staleTime: 1000 * 60 * 30, // 30 minutes
     retry: false,
+    refetchOnWindowFocus: false,
   });
 };
 
