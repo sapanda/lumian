7d6090c4d8a57eeba167429e2bfa6fc259a00233
atulmetanext <atul@getmetanext.com>
2023-07-17 16:26:57 +0530

    feat: create 404 page

--- web/src/pages/NotFound/Notfound.tsx
+++ web/src/pages/NotFound/Notfound.tsx
@@ -1,10 +1,27 @@
-
+import { useEffect } from "react";
 import { PublicContainer } from "../../components/Containers";
-
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+// import NotFoundIllustration from "../../../public/404.png"
 export default function Notfound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const location = window.location;
+    const path = location.pathname;
+    if (path !== "/404") navigate("/404");
+  }, [navigate]);
   return (
     <PublicContainer>
-      <h1>404</h1>
+      <img src="/404.png" alt="404" className="w-full max-w-[700px]" />
+      <Button
+        variant="contained"
+        onClick={() => navigate("/")}
+        sx={{
+          marginTop: "40px",
+        }}
+      >
+        Go to Home
+      </Button>
     </PublicContainer>
   );
 }
